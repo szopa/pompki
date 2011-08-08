571fbff263503a4271ec25bc035e396ad67ed8d1
bklosinski <bklosinski@bklosinski.(none)>
2011-08-08 11:09:34 +0200

    inicjalizacja projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pompki::Application.config.secret_token = '524818f9c855740233b98e071e76f2b9ea929e6eb6e6e91c351ca75a1f0a61a1726c8c175c82afb6d0e3faea0da1e37a9e5c6ea3987c8de30237166279cc879b'
