966d04a0ed099ad0ab746e7b9331e3d13162a009
szopa <bartek@szopa-laptop.(none)>
2011-08-14 01:25:31 +0200

    validation + beginning of charts

--- app/models/pushup.rb
+++ app/models/pushup.rb
@@ -1,3 +1,7 @@
 class Pushup < ActiveRecord::Base
+  validates :amount, :presence => true, :numericality => {:greater_than => 0}
   belongs_to :user
+  def without_hours
+    (created_at.to_i - created_at.to_i%1.day.to_i)*1000
+  end
 end
