aae83f4c6075eba3096451b080b65a341832cc10
szopa <bartek@szopa-laptop.(none)>
2011-09-08 02:50:42 +0200

    fixed series removing,issues #19 #12

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,7 +1,7 @@
 class Series < ActiveRecord::Base
   belongs_to :user
   has_many :pushups, :dependent => :destroy
-  accepts_nested_attributes_for :pushups, :reject_if => lambda { |a| a[:amount].blank?}
+  accepts_nested_attributes_for :pushups, :reject_if => lambda { |a| a[:amount].blank?}, :allow_destroy => true
   def without_hours
     (created_at.to_i - created_at.to_i%1.day.to_i)*1000
   end
